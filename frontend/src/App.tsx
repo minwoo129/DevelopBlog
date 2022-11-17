5f56e0fd35212ff0d05b27aa4a3431ac42cc2158
minwoo129 <key0129mw@naver.com>
2022-11-17 14:22:48 +0900

    frontend 회원가입 페이지 route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<LoginPage />} path="/*" />
+      <Route element={<RegisterPage />} path="/register" />
     </Routes>
   );
 }
