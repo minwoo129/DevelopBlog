77a8e9a4096e09d8e8f6db302a3db7291b434864
minwoo129 <key0129mw@naver.com>
2022-11-17 12:53:25 +0900

    테스트 페이지 추가

---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+type MainPageProp = {};
+const Main: FC<MainPageProp> = ({}) => {
+  return (
+    <div>
+      <h1>main</h1>
+    </div>
+  );
+};
+
+export default Main;
