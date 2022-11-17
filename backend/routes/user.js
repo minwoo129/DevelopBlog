3bc05c384f9192b74a1b86416f14d7ef8ff90d56
minwoo129 <key0129mw@naver.com>
2022-11-17 15:22:57 +0900

    회원가입, 회원조회(단건, 다수) api 설계

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,33 +8,70 @@ const router = express.Router();
 AWS.config.update(dynamodbAccessKey);
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-router.post("/", async (req, res, next) => {
-  const { email, pwd, name } = req.body;
+router.post("/login", async (req, res, next) => {});
 
+router.post("/join", async (req, res, next) => {
+  const { email, password, name } = req.body;
+  const exixtQuery = {
+    TableName: "DevelopBlog_user",
+    KeyConditionExpression: "email=:email",
+    ExpressionAttributeValues: {
+      ":email": email,
+    },
+  };
   try {
-    const hash = await bcrypt.hash(pwd, 12);
-    dynamoDB.put(
-      {
-        TableName: "devblog_user_test",
-        Item: {
-          id: v4().split("-").join(""),
-          email,
-          pwd: hash,
-          name,
-        },
+    const existUser = await dynamoDB.query(exixtQuery).promise();
+    if (existUser.Count > 0) {
+      res
+        .status(400)
+        .json({ error: true, code: 400, data: "이미 존재하는 이메일입니다." });
+      return;
+    }
+    const hash = await bcrypt.hash(password, 12);
+    const joinQuery = {
+      TableName: "DevelopBlog_user",
+      Item: {
+        email,
+        password: hash,
+        name,
       },
-      (err, data) => {
-        if (err) {
-          console.error(err);
-          return next(err);
-        }
+    };
+    const result = await dynamoDB.put(joinQuery).promise();
+    res.status(200).json({ code: 200, data: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true, code: 500, data: err });
+  }
+});
 
-        res.send("ok");
-      }
-    );
-  } catch (error) {
-    console.error(error);
-    return next(error);
+router.get("/getList", async (req, res, next) => {
+  const query = {
+    TableName: "DevelopBlog_user",
+  };
+  try {
+    const result = await dynamoDB.scan(query).promise();
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true, code: 500, data: err });
+  }
+});
+
+router.get("/getOne", async (req, res, next) => {
+  const { email } = req.query;
+  let query = {
+    TableName: "DevelopBlog_user",
+    KeyConditionExpression: "email=:email",
+    ExpressionAttributeValues: {
+      ":email": email,
+    },
+  };
+  try {
+    const result = await dynamoDB.query(query).promise();
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true, code: 500, data: err });
   }
 });
 
