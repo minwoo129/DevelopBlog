2e651d79206a0401bc0b48effa40ed73a2fca790
minwoo129 <key0129mw@naver.com>
2022-11-18 17:47:47 +0900

    frontend 회원가입 폼 가입 버튼 클릭 이벤트 처리1

--- frontend/src/components/auth/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm.tsx
@@ -15,6 +15,33 @@ const RegisterForm = () => {
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     const { email, password, passwordConfirm, isAdmin, name, adminPwd } = form;
+    if (email == "") {
+      alert("이메일을 입력해주세요.");
+      e.preventDefault();
+      return;
+    }
+    if (password == "") {
+      alert("비밀번호를 입력해주세요.");
+      e.preventDefault();
+      return;
+    }
+    if (passwordConfirm == "") {
+      alert("확인용 비밀번호를 입력해주세요.");
+      e.preventDefault();
+      return;
+    }
+    if (name == "") {
+      alert("이름을 입력해주세요.");
+      e.preventDefault();
+      return;
+    }
+
+    if (password != passwordConfirm) {
+      alert("비밀번호가 일치하지 않습니다.");
+      e.preventDefault();
+      return;
+    }
+
     e.preventDefault();
   };
   const onCheckAdmin = (value: boolean) => {
