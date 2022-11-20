e0c883df52e65b2aa3dac3c2b609a0469a3bdf20
minwoo129 <key0129mw@naver.com>
2022-11-20 16:20:49 +0900

    frontend 조건별 메뉴 ui 적용

--- frontend1/src/components/menu/MenuTemplate.tsx
+++ frontend1/src/components/menu/MenuTemplate.tsx
@@ -1,10 +1,13 @@
 import React, { FC, HTMLAttributes, MouseEvent } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./MenuTemplate.scss";
 
 interface MenuTemplateProps extends HTMLAttributes<HTMLDivElement> {}
 const MenuTemplate: FC<MenuTemplateProps> = (props) => {
   const navigate = useNavigate();
+  const loginInfo = useSelector((state: any) => state.auth.loginInfo);
+  const login = useSelector((state: any) => state.auth.login);
   const onClickTitle = (e: MouseEvent<HTMLDivElement>) => {
     navigate("/");
   };
@@ -23,8 +26,11 @@ const MenuTemplate: FC<MenuTemplateProps> = (props) => {
         <div className="menuTitle" onClick={onClickTitle}>
           <h3>DEVBLOG</h3>
         </div>
+        {login && (
+          <div className="loginInfo">{`${loginInfo.name}님, 반갑습니다.`}</div>
+        )}
         <div className="menuItem" onClick={onClickLogin}>
-          <h3>로그인</h3>
+          <h3>{login ? "로그아웃" : "로그인"}</h3>
         </div>
         <div className="menuItem" onClick={onClickMyArticle}>
           <h3>내 작성 글</h3>
