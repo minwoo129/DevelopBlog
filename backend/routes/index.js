5dd71afaab664c7be497729cc56c966704ec9b51
minwoo129 <key0129mw@naver.com>
2022-11-20 11:41:34 +0900

    test api 구성

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/test", async (req, res, next) => {
   try {
     res.status(201).json({ test: "success" });
   } catch (e) {
