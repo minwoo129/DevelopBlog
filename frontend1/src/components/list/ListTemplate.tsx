34efc765f3ec94c94f83a39e77fb9025dd9fa58d
minwoo129 <key0129mw@naver.com>
2022-11-23 18:24:44 +0900

    frontend 리스트 페이지 헤더 적용

--- frontend1/src/components/list/ListTemplate.tsx
+++ frontend1/src/components/list/ListTemplate.tsx
@@ -2,32 +2,20 @@ import React, { FC, HTMLAttributes, MouseEvent } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import invokeAPI from "../../modules/restAPI";
+import Header from "./Header";
 
 const ListTemplateBlock = styled.div`
   flex: 1;
   border: 1px solid orange;
-  color: orange;
+  background: #e9ecef;
 `;
 
 interface ListTemplateProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ListTemplate: FC<ListTemplateProps> = (props) => {
-  const dispatch = useDispatch<any>();
-
-  const tokenTest = async (e: MouseEvent<HTMLDivElement>) => {
-    try {
-      const result = await invokeAPI({ method: "get", path: "/token/test" })(
-        {}
-      );
-      console.log("tokenTest result: ", result);
-    } catch (e) {
-      console.log("tokenTest error: ", e);
-    }
-  };
-
   return (
     <ListTemplateBlock>
-      <div onClick={tokenTest}>토큰 테스트</div>
+      <Header />
     </ListTemplateBlock>
   );
 };
