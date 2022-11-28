bd8a41975654ad3106458228a68c64a5aea29c7c
minwoo129 <key0129mw@naver.com>
2022-11-28 13:14:00 +0900

    backend passport 로직 수정

--- backend/passport/index.js
+++ backend/passport/index.js
@@ -1,31 +1,17 @@
 const passport = require("passport");
 const local = require("./localStrategy");
-const AWS = require("aws-sdk");
-const dynamodbAccessKey = require("../AWS/dynamodbAccessKey");
-
-AWS.config.update(dynamodbAccessKey);
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const User = require("../models/user");
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
     done(null, user.email);
   });
   passport.deserializeUser((email, done) => {
-    const exixtQuery = {
-      TableName: "DevelopBlog_user",
-      KeyConditionExpression: "email=:email",
-      ExpressionAttributeValues: {
-        ":email": email,
-      },
-    };
-    dynamoDB.query(exixtQuery, (err, data) => {
-      if (err) {
-        done(err);
-        return;
-      }
-      done(null, data.Items[0]);
-      return;
-    });
+    User.findOne({
+      where: { email },
+    })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
   });
 
   local();
