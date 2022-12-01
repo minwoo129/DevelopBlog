003a2eab42d1ad5b92b71386ab140e6453ff05fc
minwoo129 <key0129mw@naver.com>
2022-12-01 13:24:17 +0900

    frontend 토큰 검증 api 추가

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -6,17 +6,26 @@ import MenuTemplate from "../components/menu/MenuTemplate";
 import { getCookies } from "../lib/restAPI";
 import { initializeByToken } from "../modules/actions/auth";
 import { RootState } from "../modules/reducer";
+import { tokenCheckThunk } from "../modules/thunk/auth";
 
 const MainPage = ({}) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<any>();
   const login = useSelector((state: RootState) => state.auth.login);
 
   useEffect(() => {
     if (!login) {
-      const cookie = getCookies("cookie");
-      if (cookie) dispatch(initializeByToken(cookie));
+      const cookie = getCookies("access_token");
+      if (cookie) _tokenCheck();
     }
   }, []);
+
+  const _tokenCheck = async () => {
+    try {
+      await dispatch(tokenCheckThunk({}));
+    } catch (e) {
+      console.log("MainPage _tokenCheck error: ", e);
+    }
+  };
 
   return (
     <MenuTemplate>
