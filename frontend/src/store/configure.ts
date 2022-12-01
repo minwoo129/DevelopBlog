f429bf78070f7547acf014e25a9918ad93295188
minwoo129 <key0129mw@naver.com>
2022-12-01 13:53:44 +0900

    frontend redux-logger 적용

--- frontend/src/store/configure.ts
+++ frontend/src/store/configure.ts
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../modules/reducer";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 const configure = () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer, applyMiddleware(logger, thunk));
 };
 
 export default configure;
