4092cb6eb9888a3f424e91f3f3d1f3f5cc55c543
minwoo129 <key0129mw@naver.com>
2022-12-01 13:54:48 +0900

    release 브랜치 redux-logger 제거

--- frontend/src/store/configure.ts
+++ frontend/src/store/configure.ts
@@ -1,9 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../modules/reducer";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 const configure = () => {
-  return createStore(rootReducer, applyMiddleware(logger, thunk));
+  return createStore(rootReducer, applyMiddleware(thunk));
 };
 
 export default configure;
