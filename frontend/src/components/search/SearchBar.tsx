9e0b081ab7550299f1ba8216ce92d7174ea6785b
minwoo129 <key0129mw@naver.com>
2022-12-07 23:26:35 +0900

    frontend 검색 헤더 드로어 버튼 추가

--- frontend/src/components/search/SearchBar.tsx
+++ frontend/src/components/search/SearchBar.tsx
@@ -1,5 +1,11 @@
 import React, { ChangeEvent, FC, HTMLAttributes } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { RootState } from "../../modules/reducer";
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setMenuOpen } from "../../modules/actions/menu";
 
 const SearchBarBlock = styled.div`
   background-color: #fff;
@@ -9,7 +15,12 @@ const SearchBarBlock = styled.div`
   max-width: 99%;
   align-items: center;
   justify-content: space-between;
-  padding: 1rem 1rem;
+  @media (max-width: 906px) {
+    padding: 1rem 1rem 1rem 0;
+  }
+  @media (min-width: 906px) {
+    padding: 1rem 1rem;
+  }
 `;
 
 const StyledInput = styled.input`
@@ -17,11 +28,11 @@ const StyledInput = styled.input`
   border-radius: 6px;
   padding: 0 1rem;
   border: 1px solid #6e6e6e;
-  @media (max-width: 768px) {
-    width: 70%;
+  @media (max-width: 906px) {
+    width: 60%;
   }
-  @media (min-width: 768px) {
-    width: 80%;
+  @media (min-width: 906px) {
+    width: 85%;
   }
   @media (min-width: 1400px) {
     width: 90%;
@@ -51,8 +62,23 @@ const SearchBar: FC<SearchBarProps> = ({
   onPressSearch,
   ...props
 }) => {
+  const dispatch = useDispatch<any>();
+  const isMenuVisible = useSelector(
+    (state: RootState) => state.menu.isMenuVisible
+  );
   return (
     <SearchBarBlock {...props}>
+      {!isMenuVisible && (
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge={false}
+          onClick={() => dispatch(setMenuOpen(true))}
+          style={{ marginLeft: "1rem" }}
+        >
+          <MenuIcon />
+        </IconButton>
+      )}
       <StyledInput
         value={searchTxt}
         onChange={onChangeValue}
