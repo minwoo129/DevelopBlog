a73592b04cbbafb571a0e90a49c070cc00fa3956
minwoo129 <key0129mw@naver.com>
2022-12-07 23:28:11 +0900

    frontend 불필요 페이지 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./pages/AuthPage";
 import DetailPage from "./pages/DetailPage";
-import ListPage from "./pages/ListPage";
 import MainPage from "./pages/MainPage";
 import SearchPage from "./pages/SearchPage";
 import WritePost from "./pages/WritePost";
@@ -14,7 +13,6 @@ const App = () => {
       <Route element={<MainPage />} path={"/*"} />
       <Route element={<AuthPage />} path={"/auth/login*"} />
       <Route element={<AuthPage />} path={"/auth/join*"} />
-      <Route element={<ListPage />} path={"/list*"} />
       <Route element={<SearchPage />} path={"/search*"} />
       <Route element={<WritePost />} path={"/write/*"} />
       <Route element={<DetailPage />} path={"/detail/*"} />
