808d002c273b35790a9ac2e6f052563dce02bf7c
minwoo129 <key0129mw@naver.com>
2022-12-11 12:06:55 +0900

    frontend 사용자 정보 요청 api 연결

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,8 +1,25 @@
-import React, { FC, HTMLAttributes } from "react";
+import React, { FC, HTMLAttributes, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import MenuTemplate from "../components/menu/MenuTemplate";
 import MyPageTemplate from "../components/myPage/MyPageTemplate";
+import { getUserInfoThunk } from "../modules/thunk/appInfo";
 
 const MyPage = ({}) => {
+  const dispatch = useDispatch<any>();
+
+  useEffect(() => {
+    _getUserInfo();
+  }, []);
+
+  const _getUserInfo = async () => {
+    try {
+      const result = await dispatch(getUserInfoThunk({}));
+      console.log("MyPage _getUserInfo result: ", result);
+    } catch (err) {
+      console.log("MyPage _getUserInfo error: ", err);
+    }
+  };
+
   return (
     <MenuTemplate>
       <MyPageTemplate />
