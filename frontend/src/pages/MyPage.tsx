810805179ec459ee5ff61f75adbcb469386105cc
minwoo129 <key0129mw@naver.com>
2022-12-11 13:30:54 +0900

    frontend 사용자 작성 블로그 목록 api 연결

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,14 +1,25 @@
 import React, { FC, HTMLAttributes, useEffect } from "react";
+import { batch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import MenuTemplate from "../components/menu/MenuTemplate";
 import MyPageTemplate from "../components/myPage/MyPageTemplate";
-import { getUserInfoThunk } from "../modules/thunk/appInfo";
+import { RootState } from "../modules/reducer";
+import {
+  getUserInfoThunk,
+  getUserWriteBlogsThunk,
+} from "../modules/thunk/appInfo";
 
 const MyPage = ({}) => {
   const dispatch = useDispatch<any>();
 
+  const loginInfo = useSelector((state: RootState) => state.auth.loginInfo);
+
   useEffect(() => {
-    _getUserInfo();
+    batch(() => {
+      _getUserInfo();
+      _getUserWriteBlogs();
+    });
   }, []);
 
   const _getUserInfo = async () => {
@@ -17,6 +28,22 @@ const MyPage = ({}) => {
       console.log("MyPage _getUserInfo result: ", result);
     } catch (err) {
       console.log("MyPage _getUserInfo error: ", err);
+    }
+  };
+
+  const _getUserWriteBlogs = async () => {
+    try {
+      const result = await dispatch(
+        getUserWriteBlogsThunk({
+          params: {
+            page: 1,
+            size: 20,
+          },
+        })
+      );
+      console.log("MyPage _getUserWriteBlogs result: ", result);
+    } catch (err) {
+      console.log("MyPage _getUserWriteBlogs error: ", err);
     }
   };
 
