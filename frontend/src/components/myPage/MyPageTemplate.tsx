32bb7a8edf78c780cc4d2c74c8c8fefec4e5036c
minwoo129 <key0129mw@naver.com>
2022-12-12 09:58:02 +0900

    frontend 회원정보 수정 상태값 redux 상태값으로 이동

--- frontend/src/components/myPage/MyPageTemplate.tsx
+++ frontend/src/components/myPage/MyPageTemplate.tsx
@@ -1,6 +1,8 @@
 import React, { FC, HTMLAttributes, useState } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { setAppState } from "../../modules/actions/appInfo";
 import { RootState } from "../../modules/reducer";
 import Body from "./body/Body";
 import Header from "./Header";
@@ -15,19 +17,26 @@ const MyPageTemplateBlock = styled.div`
 interface MyPageTemplateProps extends HTMLAttributes<HTMLDivElement> {}
 
 const MyPageTemplate: FC<MyPageTemplateProps> = ({ ...props }) => {
+  const dispatch = useDispatch<any>();
   const userBlogs = useSelector(
     (state: RootState) => state.appInfo.userWriteBlogs
   );
   const userInfo = useSelector((state: RootState) => state.appInfo.userInfo);
+  const isReviseUserInfo = useSelector(
+    (state: RootState) => state.appInfo.isReviseUserInfo
+  );
 
-  const [isRevise, setRevise] = useState(false);
+  const _setReviseUserInfo = (value: boolean) => {
+    dispatch(setAppState({ key: "isReviseUserInfo", value }));
+  };
+
   return (
     <MyPageTemplateBlock {...props}>
       <Header />
       <Body
         userBlogs={userBlogs}
-        isRevise={isRevise}
-        setRevise={setRevise}
+        isRevise={isReviseUserInfo}
+        setRevise={_setReviseUserInfo}
         userInfo={userInfo}
       />
     </MyPageTemplateBlock>
