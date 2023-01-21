f59ccf114791c7c633e585fc7905481a4feebd49
minwoo129 <key0129mw@naver.com>
2023-01-21 16:04:11 +0900

    frontend 검색 페이지 헤더 스타일 수정

--- frontend/src/components/search/SearchBar.tsx
+++ frontend/src/components/search/SearchBar.tsx
@@ -10,17 +10,25 @@ import { setMenuOpen } from "../../modules/actions/menu";
 const SearchBarBlock = styled.div`
   background-color: #fff;
   display: flex;
-  flex: 1;
-  max-height: 3rem;
-  max-width: 99%;
+  align-items: center;
+  justify-content: center;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+`;
+
+const SearchBarInsideGrid = styled.div`
+  @media (max-width: 700px) {
+    width: 100%;
+  }
+  @media (min-width: 700px) {
+    width: 700px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  display: flex;
   align-items: center;
   justify-content: space-between;
-  @media (max-width: 906px) {
-    padding: 1rem 1rem 1rem 0;
-  }
-  @media (min-width: 906px) {
-    padding: 1rem 1rem;
-  }
 `;
 
 const StyledInput = styled.input`
@@ -28,14 +36,14 @@ const StyledInput = styled.input`
   border-radius: 6px;
   padding: 0 1rem;
   border: 1px solid #6e6e6e;
-  @media (max-width: 906px) {
-    width: 60%;
+  @media (max-width: 700px) {
+    width: 400px;
   }
-  @media (min-width: 906px) {
-    width: 85%;
+  @media (min-width: 700px) {
+    width: 500px;
   }
-  @media (min-width: 1400px) {
-    width: 90%;
+  @media (min-width: 1200px) {
+    width: 600px;
   }
 `;
 const SearchBtn = styled.button`
@@ -68,24 +76,26 @@ const SearchBar: FC<SearchBarProps> = ({
   );
   return (
     <SearchBarBlock {...props}>
-      {!isMenuVisible && (
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge={false}
-          onClick={() => dispatch(setMenuOpen(true))}
-          style={{ marginLeft: "1rem" }}
-        >
-          <MenuIcon />
-        </IconButton>
-      )}
-      <StyledInput
-        value={searchTxt}
-        onChange={onChangeValue}
-        placeholder="검색어를 입력해주세요."
-      />
+      <SearchBarInsideGrid>
+        {!isMenuVisible && (
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge={false}
+            onClick={() => dispatch(setMenuOpen(true))}
+            style={{ marginLeft: "1rem" }}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+        <StyledInput
+          value={searchTxt}
+          onChange={onChangeValue}
+          placeholder="검색어를 입력해주세요."
+        />
 
-      <SearchBtn onClick={onPressSearch}>검색하기</SearchBtn>
+        <SearchBtn onClick={onPressSearch}>검색하기</SearchBtn>
+      </SearchBarInsideGrid>
     </SearchBarBlock>
   );
 };
