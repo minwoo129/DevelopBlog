f274bff4fb3cc21297937d8f4f386c472acb9cfb
minwoo129 <key0129mw@naver.com>
2023-06-20 20:55:04 +0900

    frontend redux 데이터 타입, 초기 데이터 그루핑

---
+++ frontend/src/redux/state/AdditionalTypes.ts
@@ -0,0 +1,19 @@
+// ================================ Auth =======================================
+export type { joinForm, loginForm, loginInfo } from "./Auth/types";
+
+// ================================ Menu =======================================
+//export type {  } from "./Menu/types";
+
+// ================================ Blog =======================================
+export type {
+  CommentType,
+  CommentUserType,
+  blogCommentsType,
+  blogDetailType,
+  blogItemType,
+  blogItemUser,
+  userWriteBlogsType,
+} from "./Blog/types";
+
+// ================================ AppInfo =======================================
+export type { userDetailInfoType, userProfileImgType } from "./AppInfo/types";
---
+++ frontend/src/redux/state/index.ts
@@ -0,0 +1,15 @@
+// ================================ Auth =======================================
+export { initialState as AuthInitialState } from "./Auth";
+export type { initialStateType as AuthInitialStateType } from "./Auth/types";
+
+// ================================ Menu =======================================
+export { initialState as MenuInitialState } from "./Menu";
+export type { initialStateType as MenuInitialStateType } from "./Menu/types";
+
+// ================================ Blog =======================================
+export { initialState as BlogInitialState } from "./Blog";
+export type { initialStateType as BlogInitialStateType } from "./Blog/types";
+
+// ================================ AppInfo =======================================
+export { initialState as AppInfoInitialState } from "./AppInfo";
+export type { initialStateType as AppInfoInitialStateType } from "./AppInfo/types";
