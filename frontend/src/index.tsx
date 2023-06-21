5797f67a3c9fde7b2cee2468274436bf2acd3888
minwoo129 <key0129mw@naver.com>
2023-06-21 23:15:33 +0900

    store 교체

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,8 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store";
 import { CookiesProvider } from "react-cookie";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
